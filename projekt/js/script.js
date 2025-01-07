a44540a7ea4dbab5afa8e340e0ebf9860f54e728
Jorge Avila <jorgeavilas@icloud.com>
2025-01-07 10:17:47 +0100

    update notifi

--- projekt/js/script.js
+++ projekt/js/script.js
@@ -52,3 +52,32 @@ document.addEventListener("DOMContentLoaded", () => {
     overlay.classList.add("hidden");
   });
 });
+
+// klocka
+const WEEK = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+function updateTime() {
+  var now = new Date();
+
+  document.getElementById("datetime").innerText =
+    now.getFullYear() +
+    "-" +
+    zeroPadding(now.getMonth() + 1, 2) +
+    "-" +
+    zeroPadding(now.getDate(), 2) +
+    " " +
+    WEEK[now.getDay()] +
+    " " +
+    zeroPadding(now.getHours(), 2) +
+    ":" +
+    zeroPadding(now.getMinutes(), 2) +
+    ":" +
+    zeroPadding(now.getSeconds(), 2);
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+function zeroPadding(num, digit) {
+  return String(num).padStart(digit, "0");
+}
