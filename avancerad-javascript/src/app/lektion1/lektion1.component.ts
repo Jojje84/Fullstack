57019174035b1a19134faccc88b3f419375dd9d3
Jorge Avila <jorgeavilas@icloud.com>
2025-03-17 11:49:17 +0100

    update

--- avancerad-javascript/src/app/lektion1/lektion1.component.ts
+++ avancerad-javascript/src/app/lektion1/lektion1.component.ts
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './lektion1.component.scss'
 })
 export class Lektion1Component {
+  public width: number = 12;
+  public height: number = 7;
+  public items: Item[] = [{ name: 'note' }];
 
+  constructor() {
+    this.createUser('John', 42);
+  }
+
+  calculateArea(width: number, height: number): number {
+    return width * height;
+  }
+
+  public createUser(name: string, age: number): User {
+    return { name: name, age: age };
+  }
+
+  public addItems(items: Item[], item: Item) {
+    items.push(item);
+    return items;
+  }
+}
+
+interface User {
+  name: string;
+  age: number;
+}
+
+interface Item {
+  name: string;
 }
