7b41262d0c457749f92ed2f1fe9545193a8d9de1
Jorge Avila <jorgeavilas@icloud.com>
2025-03-17 08:46:34 +0100

    update

--- webapp/my-react-app/vite.config.js
+++ webapp/my-react-app/vite.config.js
@@ -5,3 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+{
+  "compilerOptions": {
+    "lib": ["es6", "dom"],
+    "noImplicitAny": true,
+    "noImplicitThis": true,
+    "strictNullChecks": true
+  }
+}
